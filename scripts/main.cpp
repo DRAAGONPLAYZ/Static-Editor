ecb4a74b509758bfe02a294b68dba54d5a70cff2
DRAAGONPLAYZ <draagonplayz205@gmail.com>
2026-09-20 02:20:11 -0400

    Use dedicated Logger::log API

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -4,55 +4,43 @@
 
 #include "logger.h"
 
-#include <iostream>
-
-namespace
-{
-void log(const QString& message)
-{
-    const std::string text = message.toStdString();
-    std::cout << text << std::endl;
-    Logger::write(message);
-}
-}
-
 int main(int argc, char* argv[])
 {
     Logger::initialize();
-    log("[START] Static Editor");
+    Logger::log("[START] Static Editor");
 
-    log("[INFO] Creating QApplication...");
+    Logger::log("[INFO] Creating QApplication...");
     QApplication app(argc, argv);
-    log("[OK] QApplication created");
+    Logger::log("[OK] QApplication created");
 
-    log("[INFO] Creating main window...");
+    Logger::log("[INFO] Creating main window...");
     QMainWindow window;
-    log("[OK] Main window created");
+    Logger::log("[OK] Main window created");
 
-    log("[INFO] Setting window title...");
+    Logger::log("[INFO] Setting window title...");
     window.setWindowTitle("Static Editor");
-    log("[OK] Window title set");
+    Logger::log("[OK] Window title set");
 
-    log("[INFO] Setting window size...");
+    Logger::log("[INFO] Setting window size...");
     window.resize(1000, 650);
-    log("[OK] Window size set");
+    Logger::log("[OK] Window size set");
 
-    log("[INFO] Creating central label...");
+    Logger::log("[INFO] Creating central label...");
     auto* label = new QLabel("Static Editor - C++ / Qt / FFmpeg", &window);
     label->setAlignment(Qt::AlignCenter);
-    log("[OK] Central label created");
+    Logger::log("[OK] Central label created");
 
-    log("[INFO] Setting central widget...");
+    Logger::log("[INFO] Setting central widget...");
     window.setCentralWidget(label);
-    log("[OK] Central widget set");
+    Logger::log("[OK] Central widget set");
 
-    log("[INFO] Showing main window...");
+    Logger::log("[INFO] Showing main window...");
     window.show();
-    log("[OK] Main window shown");
+    Logger::log("[OK] Main window shown");
 
-    log("[INFO] Starting Qt event loop...");
+    Logger::log("[INFO] Starting Qt event loop...");
     const int result = app.exec();
-    log(QString("[INFO] Qt event loop ended with code: %1").arg(result));
+    Logger::log(QString("[INFO] Qt event loop ended with code: %1").arg(result));
 
     Logger::shutdown();
     return result;
