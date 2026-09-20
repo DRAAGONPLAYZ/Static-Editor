3a0cd1da0a2940261f8b646cefcba228b5f6d281
DRAAGONPLAYZ <draagonplayz205@gmail.com>
2026-09-20 17:46:55 -0400

    Set Inter as the application-wide font

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,5 +1,9 @@
 #include <QApplication>
+#include <QFont>
+#include <QFontDatabase>
+#include <QIcon>
 #include <QMainWindow>
+#include <QString>
 
 #include "logger.h"
 #include "opening_screen.h"
@@ -14,6 +18,33 @@ int main(int argc, char* argv[])
     app.setOrganizationName("Static Dev Team");
     app.setApplicationName("Static Editor");
     Logger::log("[OK] QApplication created");
+
+    Logger::log("[INFO] Loading bundled Inter fonts...");
+
+    const int regularFontId =
+        QFontDatabase::addApplicationFont(":/fonts/Inter-Regular.ttf");
+    const int mediumFontId =
+        QFontDatabase::addApplicationFont(":/fonts/Inter-Medium.ttf");
+    const int semiBoldFontId =
+        QFontDatabase::addApplicationFont(":/fonts/Inter-SemiBold.ttf");
+    const int boldFontId =
+        QFontDatabase::addApplicationFont(":/fonts/Inter-Bold.ttf");
+
+    if (regularFontId < 0 ||
+        mediumFontId < 0 ||
+        semiBoldFontId < 0 ||
+        boldFontId < 0)
+    {
+        Logger::log("[ERROR] One or more bundled Inter fonts failed to load");
+    }
+    else
+    {
+        QFont applicationFont = app.font();
+        applicationFont.setFamily("Inter");
+        app.setFont(applicationFont);
+
+        Logger::log("[OK] Inter loaded and set as application-wide font");
+    }
 
     Logger::log("[INFO] Setting application icon...");
     app.setWindowIcon(QIcon(":/images/icons/Icon.png"));
