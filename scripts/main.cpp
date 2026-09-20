07ca68ccb0198cc6092766daf8abc29d5e9f02f6
DRAAGONPLAYZ <draagonplayz205@gmail.com>
2026-09-20 03:18:09 -0400

    Use dedicated opening screen

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
-#include <QIcon>
-#include <QLabel>
 #include <QMainWindow>
-#include <QPixmap>
 
 #include "logger.h"
+#include "opening_screen.h"
 
 int main(int argc, char* argv[])
 {
@@ -23,24 +21,16 @@ int main(int argc, char* argv[])
     QMainWindow window;
     Logger::log("[OK] Main window created");
 
-    Logger::log("[INFO] Setting window title...");
     window.setWindowTitle("Static Editor");
-    Logger::log("[OK] Window title set");
-
-    Logger::log("[INFO] Setting window size...");
     window.resize(1000, 650);
-    Logger::log("[OK] Window size set");
 
-    Logger::log("[INFO] Creating loading wallpaper...");
-    auto* wallpaper = new QLabel(&window);
-    wallpaper->setPixmap(QPixmap(":/images/wallpapers/LoadWallpaper.png"));
-    wallpaper->setScaledContents(true);
-    wallpaper->setAlignment(Qt::AlignCenter);
-    Logger::log("[OK] Loading wallpaper created");
+    Logger::log("[INFO] Creating opening screen...");
+    auto* openingScreen = new OpeningScreen;
+    Logger::log("[OK] Opening screen created");
 
-    Logger::log("[INFO] Setting loading wallpaper as central widget...");
-    window.setCentralWidget(wallpaper);
-    Logger::log("[OK] Loading wallpaper set");
+    Logger::log("[INFO] Setting opening screen as central widget...");
+    window.setCentralWidget(openingScreen);
+    Logger::log("[OK] Opening screen set");
 
     Logger::log("[INFO] Showing main window...");
     window.show();
